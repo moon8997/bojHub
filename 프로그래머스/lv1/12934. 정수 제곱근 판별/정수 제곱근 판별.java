740c148e083d30e808bd5c689f5845616b4ebdcb
moon8997 <67020351+moon8997@users.noreply.github.com>
2023-09-04 22:57:08 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        Double a = Math.sqrt(n);
+        
+        if(a == a.intValue()){
+            answer = (long) Math.pow((a+1), 2);
+        } else 
+            answer = -1;
+        
+        
+        return answer;
+    }
+}

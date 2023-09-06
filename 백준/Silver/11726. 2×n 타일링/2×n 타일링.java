790d51d90c2a291dcc258102a3d0ac5c7e8c33f7
moon8997 <67020351+moon8997@users.noreply.github.com>
2023-09-06 13:15:09 +0900

    [Silver III] Title: 2×n 타일링, Time: 212 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] dp = new int[1001];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        int n = sc.nextInt();
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-1]+dp[i-2])%10007;
+        }
+
+        System.out.println(dp[n]);
+
+
+    }
+}

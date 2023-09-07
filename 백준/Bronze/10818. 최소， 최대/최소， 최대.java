df662377fd447db770198bde2f44c966c1c10980
moon8997 <67020351+moon8997@users.noreply.github.com>
2023-09-07 16:18:22 +0900

    [Bronze III] Title: 최소, 최대, Time: 2544 ms, Memory: 319608 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  // 표준 입력에서 데이터를 읽기 위한 Scanner 객체 생성
+
+        int n = sc.nextInt();
+
+        int[] arrays = new int[n];
+
+        for(int i=0; i<n; i++){
+            arrays[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arrays);
+
+        System.out.println(arrays[0] + " " + arrays[n-1]);
+    }
+}

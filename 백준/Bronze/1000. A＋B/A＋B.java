039b3176d6e278c686afd5a531d781998f37a315
moon8997 <67020351+moon8997@users.noreply.github.com>
2023-09-07 14:43:46 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  // 표준 입력에서 데이터를 읽기 위한 Scanner 객체 생성
+
+        System.out.println(sc.nextInt() + sc.nextInt());
+
+    }
+
+}

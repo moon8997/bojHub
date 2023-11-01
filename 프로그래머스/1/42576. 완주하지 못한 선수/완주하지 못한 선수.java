4554918cca75b97f6798c29195944ef92b985d85
moon8997 <67020351+moon8997@users.noreply.github.com>
2023-11-01 22:55:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 292.36 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,18 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-
-        HashMap<String, Integer> participantMap = new HashMap<>();
-        HashMap<String, Integer> completionMap = new HashMap<>();
-
-        for (String p : participant) {
-            participantMap.put(p, participantMap.getOrDefault(p, 0) + 1);
-        }
-
-        for (String c : completion) {
-            completionMap.put(c, completionMap.getOrDefault(c, 0) + 1);
-        }
-
-        for (String p : participant) {
-            if (!completionMap.containsKey(p) || participantMap.get(p) > completionMap.get(p)) {
-                answer = p;
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        int i = 0;
+        for(;i<completion.length;i++){
+            if(!participant[i].equals(completion[i]))
                 break;
-            }
         }
-
+        answer = participant[i];
         return answer;
     }
 }

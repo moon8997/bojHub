56bb6dd122d1251e7d14ddc8565998ddd7e963c0
moon8997 <67020351+moon8997@users.noreply.github.com>
2023-11-03 00:02:14 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 216 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] dp = new long[68];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        
+        for(int i=4; i<68; i++) {
+        	dp[i] = dp[i-1] + dp[i-2] + dp[i-3] + dp[i-4];
+        }
+
+        for(int i=0; i<n; i++) {
+        	int a = sc.nextInt();
+        	System.out.println(dp[a]);
+        }
+    }
+}
